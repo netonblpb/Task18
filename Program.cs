77c7e97066f619884955bfdebb634491f216451a
netonblpb <skullbog@gmail.com>
2022-02-11 14:01:16 +0300

    Task18 complete

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+Console.Clear();
+bool table(bool X, bool Y)
+{
+    return !(X | Y) == !X & !Y;
+}
+
+
+Console.WriteLine($"X - True \t Y - True \t :{table(true, true)}");
+Console.WriteLine($"X - True \t Y - False \t :{table(true, false)}");
+Console.WriteLine($"X - False \t Y - True \t :{table(false, true)}");
+Console.WriteLine($"X - False \t Y - False \t :{table(false, false)}");
